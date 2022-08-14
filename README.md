2fdd49c6a24b7f53eb0e91aa63a75fca51e7978a
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-14 17:04:24 +1000

    Add skeleton

---
+++ README.md
@@ -0,0 +1,15 @@
+# RX Analyzer
+
+A language server for rx (arcen).
+
+## Features
+
+Syntax highlighting, semantic error checking, snippets.
+
+## Requirements
+
+None
+
+## Extension Settings
+
+None
---
+++ src/extension.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/semi */
+import * as vscode from "vscode"
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "rx-analyzer" is now active!')
+
+  let disposable = vscode.commands.registerCommand(
+    "rx-analyzer.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello World from rx-analyzer!")
+    }
+  )
+
+  context.subscriptions.push(disposable)
+}
+
+export function deactivate() {}
